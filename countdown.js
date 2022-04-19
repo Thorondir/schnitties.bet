3995245db1374014e17a0a3627a104f8ede8c7a3
Joshua Steele <jsteele@nitrocade.com>
2022-04-19 14:29:14 +0930

    Wait! No IIFE required in module land.

--- countdown.js
+++ countdown.js
@@ -1,40 +1,37 @@
 import { add, formatDuration, isTuesday, nextTuesday, set, isAfter, isBefore, intervalToDuration } from "https://cdn.skypack.dev/date-fns";
 
-(() => {
-    // Blursed IIFE just for you, Ying.
-    const element = document.createElement("h1");
+const element = document.createElement("h1");
 
-    setInterval(() => {
-        const now = new Date();
+setInterval(() => {
+    const now = new Date();
 
-        // Special case for Tuesday after 2:15PM
-        const isBeforeTwoFifteen = isBefore(
-            now,
-            set(now, {
-                hours: 14,
-                minutes: 15,
-                seconds: 0,
-            })
-        );
-
-        let schnittyDay = isTuesday(now) && isBeforeTwoFifteen ? now : nextTuesday(now)
-
-        const schnittyTime = set(schnittyDay, {
-            hours: 13,
+    // Special case for Tuesday after 2:15PM
+    const isBeforeTwoFifteen = isBefore(
+        now,
+        set(now, {
+            hours: 14,
             minutes: 15,
             seconds: 0,
-        });
+        })
+    );
 
-        const endSchnittyTime = add(schnittyTime, { hours: 1 });
+    let schnittyDay = isTuesday(now) && isBeforeTwoFifteen ? now : nextTuesday(now)
 
-        const schnitties = isAfter(now, schnittyTime) && isBefore(now, endSchnittyTime) ? 
-            "Bet" : 
-            formatDuration(
-                intervalToDuration({ start: now, end: schnittyTime})
-            );
-        
-        element.innerHTML = schnitties;
-    }, 1000);
+    const schnittyTime = set(schnittyDay, {
+        hours: 13,
+        minutes: 15,
+        seconds: 0,
+    });
 
-    document.body.appendChild(element);
-})();
+    const endSchnittyTime = add(schnittyTime, { hours: 1 });
+
+    const schnitties = isAfter(now, schnittyTime) && isBefore(now, endSchnittyTime) ? 
+        "Bet" : 
+        formatDuration(
+            intervalToDuration({ start: now, end: schnittyTime})
+        );
+    
+    element.innerHTML = schnitties;
+}, 1000);
+
+document.body.appendChild(element);
